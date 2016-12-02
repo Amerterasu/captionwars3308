82574029c09df074d80257453e4e0f92a107f2ef
Phillip Le <senorkhoa@yahoo.com>
2016-12-01 21:27:53 -0700

    First prototype added. Successful connection to Google's Firebase and displays proper images.

---
+++ app_proto1/app/src/main/java/khoa/p/le/capit/MainMenu.java
@@ -0,0 +1,99 @@
+package khoa.p.le.capit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+public class MainMenu extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference dataRef;
+    private StorageReference storageRef;
+    private StorageReference userRef;
+    private FirebaseStorage storage;
+    private ImageView image;
+    private ListView previewList;
+    private ListAdapter adapter;
+    private DatabaseReference previewRef;
+
+    public static class CaptionPreview{
+        String Author,ImagePath;
+        int Likes;
+
+        public CaptionPreview(){}
+
+        public CaptionPreview(String author, String path, int likes){
+            this.Author = author;
+            this.ImagePath = path;
+            this.Likes = likes;
+        }
+
+        public String getAuthor(){return Author;}
+        public String getImagePath(){return ImagePath;}
+        public int getLikes(){return Likes;}
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        //Firebase initialization
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mAuth.getCurrentUser();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://project-426870672605395942.appspot.com/");
+        userRef = storageRef.child("Users/Khoa/Caption.png");
+        previewRef = FirebaseDatabase.getInstance().getReference("Preview/");
+
+        previewList = (ListView) findViewById(R.id.preview_list);
+
+        //user not signed in
+        if(mFirebaseUser == null){
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
+
+
+
+        adapter= new FirebaseListAdapter<CaptionPreview>(this, CaptionPreview.class, R.layout.list_caption_item, previewRef) {
+            @Override
+            protected void populateView(View v, CaptionPreview model, int position) {
+                storageRef = storage.getReferenceFromUrl(model.getImagePath());
+                ((TextView)v.findViewById(R.id.author_textView)).setText(model.getAuthor());
+                Glide.with(getApplicationContext())
+                        .using(new FirebaseImageLoader())
+                        .load(storageRef)
+                        .override(200, 200)
+                        .into((ImageView)v.findViewById(R.id.caption_preview_image));
+            }
+        };
+
+        previewList.setAdapter(adapter);
+
+    }
+}
---
+++ app_proto1/app/src/main/java/khoa/p/le/capit/RegistrationActivity.java
@@ -0,0 +1,68 @@
+package khoa.p.le.capit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private final String TAG = "REGISTRATION_ACTIVITY";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        auth = FirebaseAuth.getInstance();
+        final EditText email = (EditText) findViewById(R.id.email_editView);
+        final EditText password = (EditText) findViewById(R.id.password_editView);
+        final EditText password_confirm = (EditText) findViewById(R.id.confirm_editView);
+        Button registerButton = (Button) findViewById(R.id.signup_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailStr = email.getText().toString();
+                String passwordStr = password.getText().toString();
+                String passwordConfrim = password_confirm.getText().toString();
+                if(emailStr != null && passwordStr != null && passwordConfrim != null){
+                    if(passwordStr.equals(passwordConfrim)){
+                        auth.createUserWithEmailAndPassword(emailStr, passwordStr).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Log.v(TAG, "Created User");
+                                    startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                                    finish();
+                                }
+                                else
+                                    Log.e(TAG, "Sign In Failed");
+                            }
+                        });
+                    }else{
+                        Toast.makeText(RegistrationActivity.this, "Passwords Don't Match", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(RegistrationActivity.this, "Email/Password is empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
